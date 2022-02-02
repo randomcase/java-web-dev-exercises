507eae472ddcd89ece8e0e32fa08f89a542d425d
Zakeem Mccaleb <69187509+randomcase@users.noreply.github.com>
2022-02-02 16:28:07 -0600

    Summary

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -4,7 +4,7 @@ public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
 
-        public void setName(String name) {
-            this.name = name;
+       // public void setName(String name) {
+            //.name = name;
     }
 }
